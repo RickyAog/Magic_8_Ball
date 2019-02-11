b39197c59609d38e41581782939a33e11c195071
Ricky <richard.anderson.2021@student.lhschools.org>
2019-02-11 14:17:12 -0500

    Completed magic 8 ball

--- src/Magic_8_Ball.java
+++ src/Magic_8_Ball.java
@@ -6,29 +6,52 @@ public class Magic_8_Ball {
     //write code here
         Scanner keyboard;
         keyboard=new Scanner (System.in);
-    Float pickNumber;
-        System.out.println("Give me a number between 1 and 100");
-        pickNumber = Float.valueOf(keyboard.nextLine());
-     if(pickNumber == 1-10){
-         System.out.println("I see good fortune in your future");
-     }
+   int pickNumber;
+
+
+
+        do {
+            System.out.println("Give me a number between 1 and 100");
+            pickNumber = Integer.parseInt((keyboard.nextLine()));
+            if (pickNumber  <=10) {
+                System.out.println("I see good fortune in your future");
+            }
+           else if (pickNumber <= 20) {
+                System.out.println("You are going to be filled with sorrow in the near future");
+            }
+            else if (pickNumber <= 30) {
+                System.out.println("You are going to find the love of your life in the distant future");
+            }
+            else if (pickNumber <= 40) {
+                System.out.println("You will have nothing but depression in your near future");
+            }
+            else if (pickNumber <=50) {
+                System.out.println("you have no body there for you");
+            }
+            else if (pickNumber <= 60) {
+                System.out.print("Your future has the best outcome known to man kind");
+            }
+            else if (pickNumber <= 70) {
+                System.out.println("You are an amazing person with friends and family to love you back");
+            }
+            else if (pickNumber <= 80) {
+                System.out.println("System error 404 bad luck broke the game");
+            }
+            else if (pickNumber <= 90) {
+                System.out.println("You have no love at anytime ever");
+            }
+            else if (pickNumber <= 100) {
+                System.out.println("You are an amazing you that you can ever be, don't let yourself down");
+            }
+        }while(1+1==2);
+
+
         
         
         
         
         
         
-        
-        /*1-10good
-    11-20bad
-    21-30good
-    31-40bad
-    41-50bad
-    51-60good
-    61-70good
-    71-80bad
-    81-90bad
-    91-100good */        
-    
+
     }
 }
